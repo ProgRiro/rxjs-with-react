44e4c47a6bdd5893859dfb2af0de39ba5a96e23f
progriro <progriro@gmail.com>
2020-12-16 23:46:04 +0900

    webpack-dev-serverを設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // 開発用の設定
-  mode: 'development',
+  mode: "development",
 
   // エントリポイントとなるコード
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
 
   // バンドル後の js ファイルの出力先
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
   },
 
   // import 時に読み込むファイルの拡張子
   resolve: {
-    extensions: ['.js', '.json', '.ts', '.tsx']
+    extensions: [".js", ".json", ".ts", ".tsx"],
   },
 
   // ソースマップファイルの出力設定
-  devtool: 'source-map',
+  devtool: "source-map",
+
+  // webpack-dev-serverの設定
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    host: "0.0.0.0",
+  },
 
   module: {
     rules: [
@@ -28,16 +34,16 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/
-      }
-    ]
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+      },
+    ],
   },
 
   plugins: [
     // HTML ファイルの出力設定
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: "./src/index.html",
+    }),
+  ],
 };
