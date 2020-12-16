3bcaeb63529d0d7164b548466e2ff6db4795606d
progriro <progriro@gmail.com>
2020-12-16 23:29:46 +0900

    [add] src/index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+interface HelloProps {
+  name?: string;
+}
+
+export const Hello: React.FC<HelloProps> = ({ name }) => {
+  const helloName = name ?? 'Unknown';
+  return (
+    <b>Hello, {helloName}!</b>
+  )
+}
+
+render(<Hello name="React" />, document.getElementById('root'));
